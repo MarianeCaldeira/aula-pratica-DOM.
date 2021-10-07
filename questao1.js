825aefb20a787b8a55dd0e3a03377f4f2b75a8a3
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-07 15:27:37 -0300

    Add files via upload

---
+++ questao1.js
@@ -0,0 +1,10 @@
+function minhaFuncao(){
+    var text1 = document.getElementById("texto1").value;
+    var text2 = document.getElementById("texto2").value;
+    var somar = getDocumentById("text1").value + getDocumentById("text2").value;
+    if (text1 => 2 && text1 < 10){
+        var final = document.getElementById("result");
+        return somar;
+    }
+    console.log(somar);
+}
